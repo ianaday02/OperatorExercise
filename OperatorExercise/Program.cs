7986461de6650db9eac0e82d7c3596bc9d3c6dd5
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-07 09:33:40 -0500

    adding second exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,8 +4,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        //Exercise 2-calling the method
+        public static double AreaOfCircle(double radius)
+        {
+            return (Math.PI * radius * radius);
+        }
+
         static void Main(string[] args)
         {
+            //Exercise 1
             int a = 17;
             int b = 4;
             int quotient = a / b;
@@ -15,6 +22,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine("17/4 is 4 remainder 1");
             }
+
+            //Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with radius of {radius} is {AreaOfCircle(radius)}");
+
         }
+        
     }
 }
