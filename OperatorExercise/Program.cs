7f5283faecc0b10d346a23578f06aa453db1ab6b
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-06 21:38:56 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine("17/4 is 4 remainder 1");
+            }
+        }
+    }
+}
